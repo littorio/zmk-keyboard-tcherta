c7c64414413640f1891646b6db34d7c6d918b296
Evgeny Nikitin <evgeny.nikitin@oracle.com>
2025-08-15 20:26:12 +0200

    Some games with keyboards

---
+++ boards/arm/tcherta/a.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mv planck_rev6.dts tcherta_rev6.dts
+mv planck_rev6.keymap tcherta_rev6.keymap
+mv planck_rev6.yaml tcherta_rev6.yaml
+mv planck_rev6.zmk.yml tcherta_rev6.zmk.yml
+mv planck_rev6_defconfig tcherta_rev6_defconfig
